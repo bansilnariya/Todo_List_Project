fc494b687105a7320cb9e151a7351be28eb60213
bansilnariya <bansilnariya@gmail.com>
2024-02-16 17:03:51 +0530

    iuhipuh

--- Todo_List_Project/Model/TodoDBContext.cs
+++ Todo_List_Project/Model/TodoDBContext.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 namespace Todo_List_Project.Model
 {
-    public class TodoDBContext:DbContext
+    public class TodoDBContext : DbContext
     {
-        public TodoDBContext(DbContextOptions<TodoDBContext> options):base(options) 
+        public TodoDBContext(DbContextOptions<TodoDBContext> options) : base(options)
         {
-            
+
 
         }
-        public DbSet<Todo> Todolists{ get; set; }
+        public DbSet<Todo> Todos { get; set; }
+
+
+    }
+    class test
+    {
+        public string Id { get; set; }
+        public string    Name { get; set; }
     }
 }
